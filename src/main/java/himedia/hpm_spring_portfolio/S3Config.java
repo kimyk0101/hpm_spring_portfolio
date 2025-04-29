a9b3ff2f643990d0b2f7ff5f68189b3482ca4b7d
kimyk0101 <kimyk7131@gmail.com>
2025-04-29 12:09:31 +0900

    AWS S3 key 변경, .gitignore 추가

--- src/main/java/himedia/hpm_spring_portfolio/S3Config.java
+++ src/main/java/himedia/hpm_spring_portfolio/S3Config.java
@@ -1,12 +1,13 @@
 package himedia.hpm_spring_portfolio;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-
 
 // ✅ AWS S3와 통신하기 위한 설정 클래스
 // S3Client 객체를 스프링 전체에서 재사용할 수 있도록 Bean으로 등록해주는 역할
@@ -15,23 +16,45 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 	
+	@Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+	
+
+//    public S3Client s3Client() {
+//    	  String region = System.getenv("AWS_REGION");  // 환경 변수 읽어오기
+//          System.out.println("AWS_REGION: " + region);  // 콘솔에 로그 출력
+//        return S3Client.builder()
+//        		//	사용할 AWS 리전 
+//                .region(Region.of(System.getenv("AWS_REGION")))
+//                
+//                //	인증 정보(환경변수 참조) 
+//                .credentialsProvider(
+//                        StaticCredentialsProvider.create(
+//                                AwsBasicCredentials.create(
+//                                        System.getenv("AWS_ACCESS_KEY"),  // 액세스 키
+//                                        System.getenv("AWS_SECRET_KEY")   // 시크릿 키 
+//                                )
+//                        )
+//                )
+//                .build(); // S3Client 인스턴스 생성 
+//    }
     @Bean
     public S3Client s3Client() {
-    	  String region = System.getenv("AWS_REGION");  // 환경 변수 읽어오기
-          System.out.println("AWS_REGION: " + region);  // 콘솔에 로그 출력
         return S3Client.builder()
-        		//	사용할 AWS 리전 
-                .region(Region.of(System.getenv("AWS_REGION")))
-                
-                //	인증 정보(환경변수 참조) 
+                .region(Region.of(region))
                 .credentialsProvider(
                         StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(
-                                        System.getenv("AWS_ACCESS_KEY"),  // 액세스 키
-                                        System.getenv("AWS_SECRET_KEY")   // 시크릿 키 
-                                )
+                                AwsBasicCredentials.create(accessKey, secretKey)
                         )
                 )
-                .build(); // S3Client 인스턴스 생성 
+                .build();
     }
 }
+    
+
